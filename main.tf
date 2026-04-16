05ec5209cf61db99b5e7cafcc8b5da857ce9e4b9
Hassan Ahmed <hassanahmed17824@gmail.com>
2026-04-16 15:20:22 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,21 @@
 
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Aik mehanga instance taake report achi bane
+resource "aws_instance" "app_server" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "m5.4xlarge" 
+
+  tags = {
+    Name = "DevOps-FinOps-Project"
+  }
+}
+
+# 1TB ki storage taake cost breakdown bada dikhe
+resource "aws_ebs_volume" "storage" {
+  availability_zone = "us-east-1a"
+  size              = 1000
+  type              = "gp3"
+}
